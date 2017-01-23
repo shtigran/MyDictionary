a06478a525334dff2ea72aaf3fd154ada313e5af
shtigran <shakhbekyantigran@gmail.com>
2017-01-23 10:50:54 +0400

    My Dictionary

---
+++ MyDictionary/MyDictionary/MyDict.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace MyDictionary 
+{
+  public class MyDict<T1, T2> : IEnumerable
+      where T2 : class
+  {
+    //Fields
+    private List<T1> List1 = new List<T1>();
+    private List<T2> List2 = new List<T2>();
+
+    //Ctors
+    public MyDict()
+    {
+
+
+    }
+
+    public MyDict(T1 value) 
+    {
+
+    }
+
+    public MyDict(MyDict<T1, T2> my)
+    {
+      List1 = my.List1;
+      List2 = my.List2;
+    }
+
+    public MyDict(T1 value1, T2 value2)
+    {
+      List1.Add(value1);
+      List2.Add(value2);
+    }
+
+    //Properties
+
+    public int Count
+    {
+      get
+      {
+        return List1.Count;
+      }
+    }
+
+    //Methods
+    public void Add(T1 value1, T2 value2)
+    {
+      List1.Add(value1);
+      List2.Add(value2);
+    }
+
+    public void clear()
+    {
+      List1.Clear();
+      List2.Clear();
+    }
+
+    public bool ContainsKey(T1 value)
+    {
+      for (int i = 0; i < List1.Count; i++)
+      {
+        if (List1.Contains(value))
+          return true;
+      }
+      return false;
+
+    }
+
+    public bool ContainsValue(T2 value)
+    {
+      foreach (T2 t in List2)
+        if (t == value)
+        {
+          Console.WriteLine(t);
+          return true;
+        }
+
+      return false;
+
+    }
+
+    public void ShowDict()
+    {
+
+      for (int i = 0; i < List1.Count; i++)
+      {
+        Console.WriteLine($"The {i + 1} pair is: {List1[i]}-{List2[i]}");
+      }
+
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+      return List1.GetEnumerator();
+    }
+
+    
+    }
+
+  }
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Showing MyDictionary class:\n ");
+      // Creating two Dictionaries with constructors
+      MyDict<string, string> dict1 = new MyDict<string, string>();
+      MyDict<string, string> dict2 = new MyDict<string, string>("FirstName1", "Lastname1");
+
+      // Adding some values
+      dict2.Add("FirstName2", "Lastname2");
+      dict2.Add("FirstName3", "Lastname3");
+
+      // Showing the content of dictionary
+      dict2.ShowDict();
+
+      // Printing the count of dictionary
+      Console.WriteLine(dict2.Count);
+
+      // Creating another dictionary by copying in constructor 
+      MyDict<string, string> dict3 = new MyDict<string, string>(dict2);
+      dict3.ShowDict();
+      Console.WriteLine();
+
+      // Checking availability of values
+      Console.WriteLine(dict2.ContainsKey("FirstName1"));
+
+      // Implementing foreach loop for MyDictionary class
+      foreach(string str in dict2)
+        Console.WriteLine(str);
+
+      Console.ReadKey();
+    }
+  }
+}
